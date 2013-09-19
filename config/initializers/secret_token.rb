0d80ab61f0e708b55b00077cd210317f69ff867e
Adam Singer <adam@artforge.com>
2013-09-19 11:40:44 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubChangelog::Application.config.secret_key_base = '92b3cc5398e5ad74662d969efd19652a969b77387128f5e6d70322280417ca9b7a3d9896bc8a516b011f1bc130e2807efe9363c71bfe8a67555534ec3b1062b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubChangelog::Application.config.session_store :cookie_store, key: '_github_changelog_session'
